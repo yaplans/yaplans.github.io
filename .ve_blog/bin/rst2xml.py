ec29a62aec3760d57c1a95832c3a846375c4b197
yaplans <yaplans@mail.ru>
2018-12-25 18:08:50 +0600

    Add virtualenv and setup pelican.

---
+++ .ve_blog/bin/rst2xml.py
@@ -0,0 +1,4 @@
+#!/home/yakovlev/gh/yaplans.github.com/.ve_blog/bin/python2
+# EASY-INSTALL-SCRIPT: 'docutils==0.14','rst2xml.py'
+__requires__ = 'docutils==0.14'
+__import__('pkg_resources').run_script('docutils==0.14', 'rst2xml.py')
