c6528b9b9eccaafbe393bae2c9f24f757334e12b
yaplans <yaplans@mail.ru>
2018-12-26 16:38:29 +0600

    Renew 4

--- .compile.sh
+++ .compile.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-PRJ_DIR=/home/gh/yaplans.github.com
+PRJ_DIR=/home/yakovlev/gh/yaplans.github.com
 ##/klen/Projects/klen.github.com
 VE_DIR=$PRJ_DIR/.ve_blog
 
@@ -15,3 +15,7 @@ pelican $PRJ_DIR/source -o $PRJ_DIR -s $PRJ_DIR/source/settings.py -v
 # Return PATH
 PATH=$OLD_PATH
 export PATH
+
+##echo $VE_DIR
+##echo $OLD_PATH
+##echo $PATH
